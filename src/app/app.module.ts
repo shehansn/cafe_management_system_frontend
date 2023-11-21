6c406d4bbfcc3d94554ba7b78c4b27d66c146c97
shehan_n <shehan_n@epiclanka.net>
2023-11-21 17:36:42 +0530

    Create file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BestSellerComponent } from './components/best-seller/best-seller.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './layouts/main/main.component';
+import { ViewBillProductsComponent } from './material-component/dialog/view-bill-products/view-bill-products.component';
+import { AccordionDirectiveDirective } from './shared/accordin/accordion-directive.directive';
+import { AccordionAnchorDirectiveDirective } from './shared/accordin/accordion-anchor-directive.directive';
+import { AccordionLinkDirectiveDirective } from './shared/accordin/accordion-link-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BestSellerComponent,
+    DashboardComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ViewBillProductsComponent,
+    AccordionDirectiveDirective,
+    AccordionAnchorDirectiveDirective,
+    AccordionLinkDirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/accordin/accordion-link-directive.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAccordionLinkDirective]'
+})
+export class AccordionLinkDirectiveDirective {
+
+  constructor() { }
+
+}
